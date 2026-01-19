231827e8ba34d3b00f68a83323b738beb17b3e56
Alexis <aldavid23@gmail.com>
2026-01-19 17:44:00 -0600

    Cambio Etiqueta

--- src/Components/EnvelopeComponent/EnvelopeComponent.tsx
+++ src/Components/EnvelopeComponent/EnvelopeComponent.tsx
@@ -2,8 +2,8 @@ import React, { useRef, useState } from 'react';
 import './EnvelopeComponent.css';
 import { Container } from 'react-bootstrap';
 import musicFile from "../../asseets/JesúsAdriánRomero-MiVidaSinTi.mp3";
-import tapMe from "../../asseets/logo.gif"
-import logo from "../../asseets/Logo_Envelope.png"
+import tapMe from "../../asseets/logo2.gif"
+import logo from "../../asseets/LogoAK2.png"
 
 const EnvelopeComponent: React.FC = () => {
   const audioRef = useRef<HTMLAudioElement>(null);
--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -2,7 +2,7 @@ import "./Logo.css"
 
 const Logo = () => {
     return(
-        <img src={require("../asseets/Logo_PNG.png")} alt="Loading"/>
+        <img src={require("../asseets/Logo.png")} alt="Loading"/>
     );
 };
 
