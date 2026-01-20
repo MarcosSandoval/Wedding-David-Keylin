58439ce22c1e06fa05cb7c23e04202d1f63c5041
Alexis <aldavid23@gmail.com>
2026-01-19 21:36:02 -0600

    Color

--- src/Components/QuickMessageComponent/QuickMessageComponent.tsx
+++ src/Components/QuickMessageComponent/QuickMessageComponent.tsx
@@ -6,7 +6,7 @@ const QuickMessageComponent = ({message, color}: any) => {
     return(
         <Container fluid style={{backgroundColor: color}}>
             <Row>
-                <Col><h1 className="wedding-font-sm scale">{message}</h1></Col>
+                <Col><h1 className="wedding-font-sm scale" style={{color: "#5A775E"}}>{message}</h1></Col>
             </Row>
         </Container>
     )
