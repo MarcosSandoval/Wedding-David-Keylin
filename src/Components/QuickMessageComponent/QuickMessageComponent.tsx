97a6a8d3ae1788df6be751b9c89163cdf00393ed
Alexis <aldavid23@gmail.com>
2026-01-20 00:10:21 -0600

    Tamano letra

--- src/Components/QuickMessageComponent/QuickMessageComponent.tsx
+++ src/Components/QuickMessageComponent/QuickMessageComponent.tsx
@@ -6,7 +6,7 @@ const QuickMessageComponent = ({message, color}: any) => {
     return(
         <Container fluid style={{backgroundColor: color}}>
             <Row>
-                <Col><h1 className="wedding-font-sm scale" style={{color: "#4254AB"}}>{message}</h1></Col>
+                <Col><h1 className="wedding-font-sm scale" style={{color: "#4254AB",fontSize:"1.75rem"}}>{message}</h1></Col>
             </Row>
         </Container>
     )
